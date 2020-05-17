b8779ac80bfbece56aa952a2560ad5c816007fcf
YanaShel <yana.shelementseva@gmail.com>
2020-05-17 19:52:46 +0300

    Added Loading...

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import API from "./api";
 import UserProfile from "./components/profile/user-profile";
+import Statistics from "./components/statistics";
 import FilmsStatistics from "./components/films-statistics";
+import Loading from "./components/films/main-list/loading";
 import FilterController from "./controllers/filter-controller";
 import PageController from "./controllers/page-controller";
 import MoviesModel from "./models/movies";
 import {remove, render} from "./util/dom-util";
-
-import Statistics from "./components/statistics";
 
 const AUTHORIZATION = `Basic ufbdyf7ujkgdtejlo=`;
 const END_POINT = `https://11.ecmascript.pages.academy/cinemaddict`;
@@ -17,11 +17,12 @@ const footerStatisticsElement = document.querySelector(`.footer__statistics`);
 
 const api = new API(END_POINT, AUTHORIZATION);
 const moviesModel = new MoviesModel();
-
+const loading = new Loading();
 const filterController = new FilterController(mainElement, moviesModel);
 const pageController = new PageController(mainElement, moviesModel, api);
 
 filterController.render();
+render(mainElement, loading);
 
 let statistic = null;
 mainElement.addEventListener(`click`, (evt) => {
@@ -47,11 +48,11 @@ mainElement.addEventListener(`click`, (evt) => {
       pageController.show();
       break;
   }
-
 });
 
 api.getFilms()
   .then((films) => {
+    remove(loading);
     moviesModel.setFilms(films);
     render(headerElement, new UserProfile(films));
     pageController.render();
